368d51eb8d383e4a2ccd75093738b9e369fa61b8
Larissa Perinoto <perinotolarissa@gmail.com>
2024-06-11 18:10:34 -0300

    feat: atualiza módulos em app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AvailabilitiesModule } from './availabilities/availabilities.module';
-import { Availability } from './availabilities/entities/availability.entity';
+import { Availability } from './database/entity/availability.entity';
 import { ProfessionalsModule } from './professionals/professionals.module';
-import { SnakeNamingStrategy } from 'typeorm-snake-naming-strategy';
 import { ConfigModule } from '@nestjs/config';
+import { SchedulesModule } from './schedules/schedules.module';
+import { Schedule } from './database/entity/schedule.entity';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { AppointmentsModule } from './appointments/appointments.module';
 
 @Module({
   imports: [
@@ -18,12 +21,15 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [Availability],
+      entities: [Availability, Schedule],
       synchronize: false,
+      //migrations: ['src/database/migration/**/*.ts'],
       namingStrategy: new SnakeNamingStrategy(),
     }),
     AvailabilitiesModule,
     ProfessionalsModule,
+    SchedulesModule,
+    AppointmentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
