2e0a8373225f8e1af6f5e1c32e054c8ca971b021
Larissa Perinoto <perinotolarissa@gmail.com>
2024-06-11 13:21:56 -0300

    chore: configura variáveis de ambiente e conexão com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AvailabilitiesModule } from './availabilities/availabilities.module';
+import { Availability } from './availabilities/entities/availability.entity';
+import { ProfessionalsModule } from './professionals/professionals.module';
+import { SnakeNamingStrategy } from 'typeorm-snake-naming-strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Availability],
+      synchronize: false,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+    AvailabilitiesModule,
+    ProfessionalsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
